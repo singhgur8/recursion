74606a7a3e888c0d09d23f313441c31643f0953f
Gurjot Singh <imasingh@ucdavis.edu>
2019-10-14 19:11:02 -0700

    completed the stringify and elementsbyclass methods

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,25 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(targetClass) {
+
+  function matches(item){
+      return item === targetClass
+  } // used in the iterations to check if there is a match in the array
+
+var result = [];
+  var all = document.all;
+  for (var i =0; i<all.length; i++){
+    var someClass = all[i].className.slice().split(" "); //seperates the classes, if there are multiple
+    //and makes an array out of it so iterate through that
+
+    var exists = _.some(someClass,matches); //check to see if target exists in the classes for this func
+
+
+    if(exists){
+      result.push(all[i]);
+    }
+  }
+
+return result
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,73 @@
 
 // but you don't so you're going to write it from scratch:
 
+
+//converts
+
 var stringifyJSON = function(obj) {
-  // your code goes here
+var str = "";
+
+
+if (typeof obj === 'number' || obj === null || obj === true || obj === false){
+  str += obj
+}
+else if (typeof obj === 'string' ){
+  str += '"' + obj + '"'
+}
+else if ( Array.isArray(obj)){ // means its an array
+  str += '['
+
+  if (obj.length === 0){
+
+  }
+  else{
+    for (var i =0; i<obj.length; i++){
+
+      str += stringifyJSON(obj[i]) + ","
+    }
+    str = str.slice(0,str.length-1);
+  }
+  str += ']'
+}
+else if(typeof obj === 'function' || obj === undefined){
+  return "{}"
+}
+else{
+  var str = "{"
+  if (Object.keys(obj).length === 0){
+
+  }
+  else {
+    for(var i=0; i <Object.keys(obj).length;i++){
+      if(typeof Object.keys(obj)[i] === 'function' || Object.values(obj)[i] === undefined){
+        return "{}"
+      }
+
+      var str1 = '"'+ (stringifyJSON(Object.keys(obj)[i])) + '"'
+      //need to check if it is double quoted... then slice from 1 to second to last
+        if (str1[0] === '"') {
+          str1 = str1.slice(1,str1.length-1)
+        }
+
+      var str2 = ':'
+      var str3 = '"' + (stringifyJSON(Object.values(obj)[i])) + '"'
+      if (str3[0] === '"') {
+        str3 = str3.slice(1,str3.length-1)
+      }
+
+      var str4 =  ','
+
+      str += str1 + str2 + str3 + str4
+    } // basically if the thing returning already has its own " " around it then these are not needed, i could slice
+    str = str.slice(0,str.length-1);
+  }
+
+  str+= "}"
+}
+
+
+
+
+return str;
+
 };
